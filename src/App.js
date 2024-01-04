3ed212644016d581b6ade508b303c70fed092b65
Deepal <raval9561@saskpolytech.ca>
2024-01-04 14:10:27 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import TodoForm from './componants/TodoForm';
+
+function App() {
+  return (
+    <div className="todo-app">
+      <TodoForm/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componants/ToDoList.js
@@ -0,0 +1,16 @@
+import React ,{useState}from 'react'
+
+function ToDoList() {
+    const[input,setInput]=useState('');
+
+  return (
+   <form className="todo-form">
+    <input type="text" placeholder="Add a todo" value={input} name="text" className="todo-input"/>
+    <button className="todo-button">Add todo</button>
+    </form>
+  )
+}
+
+export default ToDoList
+
+
